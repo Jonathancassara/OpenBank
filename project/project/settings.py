3a36fba34ea54c22ffe4cd3705f36b84cdfaa096
JONATHAN CASSARA <37684306+Jonathancassara@users.noreply.github.com>
2023-10-29 17:59:08 +0100

    update
    test index

--- project/project/settings.py
+++ project/project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'client',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nom_de_la_base_de_donnees',
+        'USER': 'nom_utilisateur',
+        'PASSWORD': 'mot_de_passe',
+        'HOST': 'localhost',  # L'emplacement de la base de données MySQL (généralement 'localhost')
+        'PORT': '3306',       # Port MySQL par défaut
     }
 }
 
